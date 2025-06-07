a30e904a158a1e665f1ca621a04de1f83856a7f2
rotteralec <rotteralec@gmail.com>
2025-06-06 21:33:03 -0400

    Added gui library and gui base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiContextPass};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin { enable_multipass_for_primary_context: true })
+        .add_systems(EguiContextPass, ui_example_system)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
 }
